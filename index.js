d42c9f552007f3ff6d58df5ce2591ae51de6cdd8
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2023-08-13 03:07:17 +0700

    feat: adding origin cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import r_address from "./src/routers/r_address.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: `http://localhost:${PORT}`}));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
