ec7431f8d158f21dd54a6b5071bf14d183cc9661
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2023-08-13 15:34:26 +0700

    feat: add methos at cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import r_address from "./src/routers/r_address.js";
 
 const app = express();
 
-app.use(cors({origin: `http://localhost:${PORT}`}));
+app.use(cors({origin: `http://localhost:${PORT}`, methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']}));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
