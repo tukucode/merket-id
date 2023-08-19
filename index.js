d3d7cf611d5aec95ad9224360048a9b341f7e8bc
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2023-08-19 08:21:41 +0700

    fix: options and define cors

--- index.js
+++ index.js
@@ -11,8 +11,26 @@ import r_checkouts from "./src/routers/r_checkouts.js";
 import r_address from "./src/routers/r_address.js";
 
 const app = express();
+// app.use(cors({origin: `http://localhost:${PORT}`}));
 
-app.use(cors({origin: `http://localhost:${PORT}`}));
+const whitelist = [`http://localhost:${PORT}`, 'https://market-id-fe.vercel.app']
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.options('/*', cors(corsOptionsDelegate), (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+})
+
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
